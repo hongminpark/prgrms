2e64ece50c244780b4cafe3009a99020ba52ebee
hongminpark <zhdals1026@gmail.com>
2019-09-01 22:09:41 +0900

    Update 출처

--- search_dfs_bfs/lv2_타겟넘버.py
+++ search_dfs_bfs/lv2_타겟넘버.py
@@ -1,3 +1,4 @@
+# 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/courses/30/lessons/43165
 def solution(numbers, target):
     if not numbers and target == 0:
         return 1
--- search_dfs_bfs/lv3_네트워크.py
+++ search_dfs_bfs/lv3_네트워크.py
@@ -1,3 +1,4 @@
+# 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/courses/30/lessons/43162
 def solution(n, computer):
     answer = 0
     visited = [0] * n

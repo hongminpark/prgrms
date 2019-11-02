6b36f9b8f90dcb9640bfcf3b2c76463c98ad2e84
hongminpark <zhdals1026@gmail.com>
2019-11-02 19:08:32 +0900

    Refactor prob5.py(Unsolved, 55/100)

--- 2020kakao/prob5.py
+++ 2020kakao/prob5.py
@@ -1,35 +1,40 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60061
 
 def solution(n, build_frames):
-    frames = set()
-    for x, y, a, b in build_frames:
-        if b == 1:
-            if available(x, y, a, frames):
-                frames.add((x, y, a))
-        else:
-            if a == 0:
-                if available(x, y + 1, 0, frames) and available(x - 1, y + 1, 1, frames) and available(x, y + 1, 1, frames):
-                    frames -= {(x, y, a)}
+    global N
+    N = n
+    built_frames = set()
+    for frame in build_frames:
+        if available(frame, built_frames):
+            if frame[3]:
+                built_frames.add((frame[0], frame[1], frame[2]))
             else:
-                frames_ = frames - {(x, y, a)}
-                if available(x - 1, y, 1, frames_) and available(x + 1, y, 1, frames_) \
-                        and available(x, y, 0, frames_) and available(x + 1, y, 0, frames_):
-                    frames = frames_
-    frames = [[x, y, z] for x, y, z in frames]
-    frames.sort(key=lambda x: (x[0], x[1], x[2]))
-    return frames
+                built_frames.remove((frame[0], frame[1], frame[2]))
 
+    built_frames = [[x, y, z] for x, y, z in built_frames]
+    built_frames.sort(key=lambda x: (x[0], x[1], x[2]))
+    return built_frames
 
-def available(x, y, a, frames):
-    if y == 0:
-        return True
+def available(frame, built_frames):
+    x, y, a, b = frame[0], frame[1], frame[2], frame[3]
+    if b:
+        if a:
+            if ((x, y-1, 0) in built_frames or (x+1, y-1, 0) in built_frames) or \
+             ((x - 1, y, 1) in built_frames and (x + 1, y, 1) in built_frames):
+                return True
+        else:
+            if y == 0 or (x - 1, y, 1) in built_frames or (x, y, 1) in built_frames or (x, y - 1, 0) in built_frames:
+                return True
+        return False
 
-    if a == 0:
-        if (x - 1, y, 1) in frames or (x, y, 1) in frames or (x, y - 1, 0) in frames:
-            return True
     else:
-        if ((x, y - 1, 0) in frames or (x + 1, y - 1, 0) in frames) or \
-                ((x - 1, y, 1) in frames and (x + 1, y, 1) in frames):
-            return True
+        tmp_frames = built_frames - {(x, y, a)}
+        if a:
+            nexts = [(x-1, y, 1), (x+1, y, 1), (x-1, y, 1), (x+1, y, 0)]
+        else:
+            nexts = [(x-1, y+1, 1), (x, y+1, 1), (x, y+1, 0)]
 
-    return False
+        for x, y, a in nexts:
+            if (x, y, a) in tmp_frames and not available((x, y, a, 1), tmp_frames - {(x, y, a)}):
+                return False
+        return True

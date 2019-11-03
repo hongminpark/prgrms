61b3f057b32ad588b5df6c8f4003d1a295bde2eb
hongminpark <zhdals1026@gmail.com>
2019-11-03 23:06:00 +0900

    Update prob4(Effectiveness Unsolved)

---
+++ 2020kakao/prob4.py
@@ -0,0 +1,50 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60060
+
+class Trie:
+    def __init__(self):
+        self.head = {}
+
+    def insert(self, word):
+        cur = self.head
+        for char in word:
+            cur.setdefault(char, {})
+            cur = cur[char]
+        cur["*"] = True
+
+    def query(self, query):
+        cur = self.head
+        count = 0
+        for idx, char in enumerate(query):
+            if char == "?":
+                stack = list(cur.values())
+                for _ in range(len(query) - idx - 1):
+                    tmp = []
+                    for node in stack:
+                        if type(node) is dict:
+                            tmp += node.values()
+                    stack = tmp
+                return sum([1 for item in stack if type(item) is dict and "*" in item])
+            elif char not in cur:
+                return count
+            else:
+                cur = cur[char]
+
+    def get_value(self):
+        return self.head
+
+
+def solution(words, queries):
+    trie = Trie()
+    trie_reversed = Trie()
+    for word in words:
+        trie.insert(word)
+        trie_reversed.insert(word[::-1])
+
+    result = []
+    for query in queries:
+        if query.startswith("?"):
+            result.append(trie_reversed.query(query[::-1]))
+        else:
+            result.append(trie.query(query))
+
+    return result
